dd71ad265565fb154ac8caf1acf20610a5d82348
220064@YSE <220064md@yse-c.net>
2024-02-07 09:52:11 +0900

    load balance

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -3,7 +3,7 @@ import { ClipLoader } from 'react-spinners';
 const Loading = () => {
     return (
         <div className="flex justify-center items-center m-5">
-            <ClipLoader size={40} color="#2ab393" />
+            <ClipLoader size={50} color="#2ab393" />
         </div>
     )
 }
